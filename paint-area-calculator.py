ea69c0f667c0192a4f70016f4f2ed507639915ed
Saroj Pradhan <79797597+saroj1017@users.noreply.github.com>
2021-03-01 02:04:30 +0530

    Create paint-area-calculator.py

---
+++ paint-area-calculator.py
@@ -0,0 +1,16 @@
+#You are painting a wall. The instructions on the paint can says that
+#1 can of paint can cover 5 square meters of wall. Given a random height and
+#width of wall, calculate how many cans of paint you'll need to buy.
+
+
+import math
+def paint_calc(height,width,cover):
+   #eval_this=round((height*width)/cover)
+   print(f'{math.ceil((height*width)/cover)} cans are required')
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+# below parameters are keyword arguments
+paint_calc(height=test_h, width=test_w, cover=coverage)
