6d337b6dd6bb4be6de1addff2b4635b97d5cbcca
Saroj Pradhan <79797597+saroj1017@users.noreply.github.com>
2021-03-01 02:52:04 +0530

    Update aliter.py

--- caesar-cipher/aliter.py
+++ caesar-cipher/aliter.py
@@ -1,27 +1,23 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-shift=shift  % 25
-
 
 def caesar(text,shift):#hello   # 2
+    
     #cipher_text=''
-    d_text=''
+    d_text = ''
+    
     for i in text:
-      
+        
       if i in alphabet:
         
-        if direction=='encode':
+        if direction == 'encode':
           if alphabet.index(i) + shift > 25:
               d_text=d_text + alphabet[shift-(alphabet.index('z')-alphabet.index(i))-1]
           else:
               d_text= d_text + alphabet[alphabet.index(i) + shift]
-              
-        if direction=='decode':
+                
+        if direction == 'decode':
           if alphabet.index(i)-shift < 0:
               d_text = d_text + alphabet[alphabet.index('z') +alphabet.index(i) -shift+1]
           else:
@@ -29,9 +25,19 @@ def caesar(text,shift):#hello   # 2
       else:
         d_text+=i
     print(d_text)
-    #print(cipher_text)
+ 
+game = True    
+while game:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
-caesar(text,shift)
+  shift=shift  % 25
+  caesar(text,shift)
+  result = input('Enter Yes or no \n')
+  if result == 'no':
+    game=False
+    print('bye bye')
 
 
 # example output
@@ -39,16 +45,20 @@ caesar(text,shift)
 Type 'encode' to encrypt, type 'decode' to decrypt:
 encode
 Type your message:
-eureka i got it
+made in india
 Type the shift number:
-4
-iyvioe m ksx mx
-
+3
+pdgh lq lqgld
+Enter Yes or no 
+yes
 Type 'encode' to encrypt, type 'decode' to decrypt:
 decode
 Type your message:
-iyvioe m ksx mx
+pdgh lq lqgld
 Type the shift number:
-4
-eureka i got it
+3
+made in india
+Enter Yes or no 
+no
+bye bye
 '''
