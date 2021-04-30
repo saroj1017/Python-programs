ed712cc2e73234f1784cb8dab673198d0ed38933
Saroj Pradhan <79797597+saroj1017@users.noreply.github.com>
2021-04-30 16:48:10 +0530

    Create question_model.py

---
+++ A-QUIZ-GAME-USING-OOPS/question_model.py
@@ -0,0 +1,7 @@
+" The structure of the question will have a question text and its corresonding answer "
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
